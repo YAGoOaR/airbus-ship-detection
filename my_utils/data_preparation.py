512bf5378efcef3492735188746a647621359d2c
Yegor Grybenko <55108635+YAGoOaR@users.noreply.github.com>
2024-07-15 02:19:28 +0300

    Update and comment encoding and data preparation

--- my_utils/data_preparation.py
+++ my_utils/data_preparation.py
@@ -4,22 +4,28 @@ import cv2
 from keras.utils import Sequence
 import pandas as pd
 import numpy as np
-from encoding import decode_RLE
+from my_utils.encoding import decode_RLE
 
-import albumentations as A
-from sklearn.model_selection import train_test_split
+import albumentations as A # Augmentation
+from sklearn.model_selection import train_test_split # Splitting to train and validation
 
+# Let's use simple augmentation - only flipping vertically and horizontally
+# I decided to not to use complex augmentations, because the data is already various enough
+# We can use brightness/gamma/hue and geometric augmentations to further improve the score 
 transform = A.Compose([
     A.HorizontalFlip(p=0.5),
     A.VerticalFlip(p=0.5),
 ])
 
-def load_image(path: str, size: tuple[int, int]):
+# Get normalized and resized BGR image
+def load_image(path: str, size: tuple[int, int]) -> np.ndarray:
     img = cv2.imread(path)
     img = cv2.resize(img, size)
     img = img / 255.0
     return img
 
+# We need a generator to load and send data by batches.
+# Obviously, we can't load such dataset entirely in our GPU memory.
 class DataGenerator(Sequence):
     def __init__(
             self, 
@@ -34,43 +40,54 @@ class DataGenerator(Sequence):
         self.image_size = image_size
         self.data = data
 
+    # Number of batches
     def __len__(self):
         return int(len(self.data) / self.batch_size)
-
-    def get_img_path(self, name: str):
+    
+    # Join data folder with image name
+    def get_img_path(self, name: str) -> str:
         return os.path.join(self.image_folder, name)
 
+    # Merges different ship masks into one mask (for each image)
     @staticmethod
     def merge_masks(masks: np.ndarray, imsize: tuple[int, int]) -> np.ndarray[float]:
         if len(masks) == 0:
             return np.zeros((imsize), dtype=float)
         else:
-            return np.sum([cv2.resize(m, imsize) for m in masks], dtype=float, axis=0)
+            return np.sum(masks, dtype=float, axis=0)
 
+    # Get a single batch
     def __getitem__(self, index: int):
-        batch_data = self.data[index * self.batch_size:(index + 1) * self.batch_size]
+        batch_data = self.data[index * self.batch_size:(index + 1) * self.batch_size] # get data by index offset
 
-        X = (self.get_img_path(name) for name in batch_data.index)
-        X = np.array([*map(lambda path: load_image(path, self.image_size), X)])
+        # Process input images (X) and corresponding masks (Y)
+        X = (self.get_img_path(name) for name in batch_data.index) # get paths
+        X = map(lambda path: load_image(path, self.image_size), X) # load images
+        X = np.array([*X]) # Compute list, put into array
 
-        Y = ([decode_RLE(m) for m in masks] for masks in batch_data)
-        Y = np.array([self.merge_masks(masks, self.image_size) for masks in Y])        
+        Y = ([decode_RLE(mask, out_shape=self.image_size) for mask in masks] for masks in batch_data) # Decode each mask
+        Y = (self.merge_masks(masks) for masks in Y) # Merge masks from same images
+        Y = np.array([*Y])
 
-        transformed = transform(image=X, mask=Y)
+        transformed = transform(image=X, mask=Y) # Augmentation transformation
 
         return transformed['image'], transformed['mask']
 
+# Group ship masks by images
 def group_masks(df: pd.DataFrame) -> pd.DataFrame:
 
     def masks_to_list(masks):
         lst = list(masks)
+        # Empty segmented images have NaN values, so they should be filtered out:
         return lst if lst != [np.nan] else []
 
     return df.groupby('ImageId')['EncodedPixels'].agg(masks_to_list)
 
+# Transform mask data to use for training and form training and validation sets
 def split_data(data: pd.DataFrame, empty_image_ratio:float=None, test_size:float=0.2, random_state:int=42) -> tuple[pd.DataFrame, pd.DataFrame]:
-    masks = group_masks(data)
+    masks = group_masks(data) # Group ship masks by images
 
+    # Separate data with and without ships
     mask_has_ships = masks.map(lambda x: len(x) > 0)
     with_ships = masks[mask_has_ships]
     without_ships = masks[~mask_has_ships]
@@ -79,9 +96,11 @@ def split_data(data: pd.DataFrame, empty_image_ratio:float=None, test_size:float
         total_count = masks.shape[0]
         without_ships = without_ships.iloc[:int(total_count * empty_image_ratio)]
     
+    # Combine the data
     masks = pd.concat([with_ships, without_ships], axis=0)
 
-    ship_counts = masks.map(lambda mask_list: len(mask_list))
+    # Get ship count to stratify the data
+    ship_counts = masks.map(len)
 
     train, test = train_test_split(
         masks,
--- my_utils/encoding.py
+++ my_utils/encoding.py
@@ -1,21 +1,36 @@
 import numpy as np
+import cv2
 
-def encode_RLE(img: np.ndarray) -> str:
-    flat_img = np.concatenate([[0], img.T.flatten(), [0]])
-    runs = np.where(flat_img[1:] != flat_img[:-1])[0] + 1
-    runs[1::2] -= runs[::2]
-    return ' '.join(str(x) for x in runs)
+# Encode segmentation masks to RLE (Run-length encoding)
+def encode_RLE(img: np.ndarray) -> str:  
+    # We can represent flat image as f(x) where x is pixel index 
+    f_x = np.concatenate([[0], img.T.flatten()]) # f(x)
+    f_x_plus_1 = np.concatenate([img.T.flatten(), [0]]) # f(x+1)
 
-def decode_RLE(mask_rle: str, shape: tuple[int, int]=(768, 768)) -> np.ndarray:
-    image = np.zeros(shape[0] * shape[1], dtype=np.uint8)
+    deltas, _ = np.where(f_x != f_x_plus_1) # get pixel indices where mask value changes
+    runs = deltas + 1 # shift numeration
+    runs[1::2] -= runs[::2] # subtract run starts from ends (get run lengths)
+    return ' '.join(str(x) for x in runs) # join into a string
+
+# Decode masks from RLE
+def decode_RLE(
+        mask_rle: str,
+        encoding_shape: tuple[int, int] = (768, 768),
+        out_shape: tuple[int, int] = (768, 768),
+    ) -> np.ndarray:
+
+    image = np.zeros(encoding_shape[0] * encoding_shape[1], dtype=np.uint8) # empty mask
     
+    # Parse starts and lengths, get run ends
     runs = mask_rle.split()
-
-    starts = np.asarray(runs[0:][::2], dtype=int) -1
+    starts = np.asarray(runs[0:][::2], dtype=int) - 1
     lengths = np.asarray(runs[1:][::2], dtype=int)
     ends = starts + lengths
 
+    # for each run, draw corresponding pixels in the mask
     for s, e in zip(starts, ends):
         image[s:e] = 1
 
-    return image.reshape(shape).T
+    # Get desired shape
+    image = image.reshape(encoding_shape).T
+    return cv2.resize(image, out_shape)
