cd62c2b4d4eccc1ed3832cc6a69ad2c73f8442df
Yegor Grybenko <55108635+YAGoOaR@users.noreply.github.com>
2024-07-15 02:36:56 +0300

    Add more comments

--- my_utils/plotting.py
+++ my_utils/plotting.py
@@ -1,12 +1,14 @@
 
 import matplotlib.pyplot as plt
 
+# Draw training performance results
 def save_history_plot(history, path: str = './training_results.png'):
 
     plt.figure(figsize=(16, 5))
 
     epochs = len(history.history['loss'])
 
+    # Loss (based on Dice coefficient)
     plt.subplot(1, 2, 1)
     plt.plot(range(epochs), history.history['loss'], label='Loss - training')
     plt.plot(range(epochs), history.history['val_loss'], label='Loss - validation')
@@ -15,6 +17,7 @@ def save_history_plot(history, path: str = './training_results.png'):
     plt.ylabel('Loss')
     plt.legend()
 
+    # The dice coefficient itself
     plt.subplot(1, 2, 2)
     plt.plot(range(epochs), history.history['dice_coefficient'], label='Dice coefficient - training')
     plt.plot(range(epochs), history.history['val_dice_coefficient'], label='Dice coefficient - validation')
