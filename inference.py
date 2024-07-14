860d52334b703578430e7fc22da2abecaabfeed9
Yegor Grybenko <55108635+YAGoOaR@users.noreply.github.com>
2024-07-15 01:16:12 +0300

    Comment and update inference.py

--- inference.py
+++ inference.py
@@ -1,57 +1,71 @@
-import os
-import tensorflow as tf
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
-
-from my_utils.custom_loss import dice_coefficient, dice_coef_loss, IoU
-from my_utils.data_preparation import load_image
+import tensorflow as tf, numpy as np
 from keras.saving import load_model
+import cv2
+import matplotlib.pyplot as plt
+import os, random
 
-import random
+from my_utils.custom_loss import dice_coefficient, dice_coef_loss, IoU # Custom loss and metrics
+from my_utils.data_preparation import load_image # data preprocessing
 
+# Path to data
 DATASET = './airbus-ship-detection'
+TEST = f'{DATASET}/test_v2'
 
+# We are not interested in empty images with water only
+# That's why i found and defined the images in the test dataset (first 100 samples) that have visible ships:
+test_images_with_ships = [8, 10, 40, 43, 44, 46, 47, 50, 60, 64, 66, 67, 68, 74, 75, 76, 98]
+
+# Custom objects (that were used when compiling a model) are needed to be specified to restore the model.
 custom_objects = {
     "IoU": IoU,
     "dice_coefficient": dice_coefficient,
     "dice_coef_loss": dice_coef_loss
 }
 
+# Loading the model using custom metrics
 model: tf.keras.Model = load_model("model.keras", custom_objects=custom_objects)
 
+# Use same image height and width as in the model's input layer
 image_size = model.input.shape[1:3]
 
-TEST = f'{DATASET}/test_v2'
+# Let's show 6 random test images
+images_to_show = 6
 
-def load_input(image_path):
-    img = load_image(image_path, image_size)
-    img = np.expand_dims(img, axis=0)
-    return img
+# Select random images with ships
+selected_indices = test_images_with_ships.copy()
+random.shuffle(selected_indices)
+selected_indices = selected_indices[:images_to_show]
 
-test_images_with_ships = [8, 10, 40, 43, 44, 46, 47, 50, 60, 64, 66, 67, 68, 74, 75, 76, 98]
+# Get their paths
+images = [os.path.join(TEST, os.listdir(TEST)[idx]) for idx in selected_indices]
 
-columns = 2
-rows = 6
-fig, axs = plt.subplots(rows, columns, figsize=(7, 12))
+# Display images in 2 columns
+# Left column - input images
+# Right column - predicted masks
+def model_demo(images: list[str], model: tf.keras.Model, count: int = 6) -> None:
+    columns = 2
+    fig, axs = plt.subplots(count, columns, figsize=(7, 12))
 
-selected_images = random.shuffle(test_images_with_ships)[:rows]
+    for i, img_path in enumerate(images[:count]):
+        # Prepare image
+        img = load_image(img_path, image_size) 
+        img_rgb = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2RGB) # Convert to RGB (the model uses BGR)
+        input_image = np.expand_dims(img, axis=0) # Add batch dimension
 
-for i, img_idx in enumerate(selected_images):
-    img_path = os.path.join(TEST, os.listdir(TEST)[img_idx])
-    output_mask: np.ndarray = model.predict(load_input(img_path), verbose=0)
-    output_mask = (output_mask > 0.3).astype(int)
+        output_mask: np.ndarray = model.predict(input_image, verbose=0) # Predict ships
+        output_mask = (output_mask > 0.3).astype(int) # Set pixel confidence threshold to 0.3
 
-    img = load_image(img_path, image_size)
-    img_rgb = cv2.cvtColor(np.float32(img), cv2.COLOR_BGR2RGB)
+        mask = np.squeeze(output_mask, axis=0) # Remove batch dimension
 
-    mask = np.squeeze(output_mask, axis=0)
+        # Display image and predicted ships
+        axs[i, 0].imshow(img_rgb)
+        axs[i, 0].axis("off")
+        
+        axs[i, 1].imshow(mask)
+        axs[i, 1].axis('off')
 
-    axs[i, 0].imshow(img_rgb)
-    axs[i, 0].axis("off")
-    
-    axs[i, 1].imshow(mask)
-    axs[i, 1].axis('off')
+    fig.subplots_adjust(hspace=0.01) # Make gaps between images smaller
+    plt.show()
 
-fig.subplots_adjust(wspace = 0, hspace=0.01)
-plt.show()
+# Launch the demo
+model_demo(images, model=model, count = images_to_show)
